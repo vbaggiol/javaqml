d8b3baf875ee5283cf96bfa96dc9c90c90ab583d
vbaggiol <vbaggiol@gmail.com>
2018-09-16 15:19:52 +0200

    Modified to use generation of QMetaObject

--- Main.java
+++ Main.java
@@ -1,35 +1,42 @@
+import java.beans.IntrospectionException;
+
 public class Main {
 
     public static class User extends QObject {
         static {
-            DOtherSideJNI.SignalDefinition[] signals = new DOtherSideJNI.SignalDefinition[1];
-            signals[0] = new DOtherSideJNI.SignalDefinition();
-            signals[0].name = "nameChanged";
-            signals[0].parameters = new DOtherSideJNI.ParameterDefinition[0];
-
-            DOtherSideJNI.SlotDefinition[] slots = new DOtherSideJNI.SlotDefinition[2];
-            slots[0] = new DOtherSideJNI.SlotDefinition();
-            slots[0].name = "name";
-            slots[0].returnMetaType = DOtherSideJNI.MetaType.String.value();
-            slots[0].parameters = new DOtherSideJNI.ParameterDefinition[0];
-
-            slots[1] = new DOtherSideJNI.SlotDefinition();
-            slots[1].name = "setName";
-            slots[1].returnMetaType = DOtherSideJNI.MetaType.Void.value();
-            slots[1].parameters = new DOtherSideJNI.ParameterDefinition[1];
-            slots[1].parameters[0] = new DOtherSideJNI.ParameterDefinition();
-            slots[1].parameters[0].name = "value";
-            slots[1].parameters[0].metaType = DOtherSideJNI.MetaType.String.value();
-
-            DOtherSideJNI.PropertyDefinition[] properties = new DOtherSideJNI.PropertyDefinition[1];
-            properties[0] = new DOtherSideJNI.PropertyDefinition();
-            properties[0].name = "name";
-            properties[0].metaType = DOtherSideJNI.MetaType.String.value();
-            properties[0].readSlot = "name";
-            properties[0].writeSlot = "setName";
-            properties[0].notifySignal = "nameChanged";
-
-            staticMetaObject = new QMetaObject(QObject.staticMetaObject, "User", signals, slots, properties);
+//            DOtherSideJNI.SignalDefinition[] signals = new DOtherSideJNI.SignalDefinition[1];
+//            signals[0] = new DOtherSideJNI.SignalDefinition();
+//            signals[0].name = "nameChanged";
+//            signals[0].parameters = new DOtherSideJNI.ParameterDefinition[0];
+//
+//            DOtherSideJNI.SlotDefinition[] slots = new DOtherSideJNI.SlotDefinition[2];
+//            slots[0] = new DOtherSideJNI.SlotDefinition();
+//            slots[0].name = "name";
+//            slots[0].returnMetaType = DOtherSideJNI.MetaType.String.value();
+//            slots[0].parameters = new DOtherSideJNI.ParameterDefinition[0];
+//
+//            slots[1] = new DOtherSideJNI.SlotDefinition();
+//            slots[1].name = "setName";
+//            slots[1].returnMetaType = DOtherSideJNI.MetaType.Void.value();
+//            slots[1].parameters = new DOtherSideJNI.ParameterDefinition[1];
+//            slots[1].parameters[0] = new DOtherSideJNI.ParameterDefinition();
+//            slots[1].parameters[0].name = "value";
+//            slots[1].parameters[0].metaType = DOtherSideJNI.MetaType.String.value();
+//
+//            DOtherSideJNI.PropertyDefinition[] properties = new DOtherSideJNI.PropertyDefinition[1];
+//            properties[0] = new DOtherSideJNI.PropertyDefinition();
+//            properties[0].name = "name";
+//            properties[0].metaType = DOtherSideJNI.MetaType.String.value();
+//            properties[0].readSlot = "name";
+//            properties[0].writeSlot = "setName";
+//            properties[0].notifySignal = "nameChanged";
+//
+//            staticMetaObject = new QMetaObject(QObject.staticMetaObject, "User", signals, slots, properties);
+            try {
+                staticMetaObject = new QmlMetaObjectBuilder(User.class).build();
+            } catch (IntrospectionException e) {
+                e.printStackTrace();
+            }
         }
 
         public User(String name) {
@@ -61,10 +68,13 @@ public class Main {
         }
 
         private static QMetaObject staticMetaObject;
+        
+        @QProperty
         private String name;
     }
 
     public static void main(String[] args) {
+        System.loadLibrary("DOtherSideJNI");
         QApplication app = new QApplication();
         QQmlApplicationEngine engine = new QQmlApplicationEngine();
         QQmlContext context = engine.rootContext();
